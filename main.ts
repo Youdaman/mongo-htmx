3745a59b180187439a51692012fbaed8273106ef
Stew Heckenberg <stewart.heckenberg@gmail.com>
2025-04-07 07:34:51 +0800

    two streams

--- main.ts
+++ main.ts
@@ -38,31 +38,24 @@ app.get('/items', async (c) => {
   return c.html(html);
 });
 
-// let id = 0
-
-// app.get('/sse', (c) => {
-//   return streamSSE(c, async (stream) => {
-//     while (true) {
-//       const message = `It is ${new Date().toISOString()}`
-//       await stream.writeSSE({
-//         data: message,
-//         event: 'EventName',
-//         // id: String(id++),
-//       })
-//       await stream.sleep(1000)
-//     }
-//   })
-// })
+let id = 0
+app.get('/sse', (c) => {
+  return streamSSE(c, async (stream) => {
+    while (true) {
+      const message = `It is ${new Date().toISOString()}`
+      await stream.writeSSE({
+        data: message,
+        event: 'EventName',
+        id: String(id++),
+      })
+      await stream.sleep(1000)
+    }
+  })
+})
 
 app.get('/stream', (c) => {
   console.log('Received GET request to /stream for SSE');
   const changeStream = collection.watch();
-  // changeStream.on('error', (error) => {
-  //   console.error('Change stream error:', error);
-  // });
-  // changeStream.on('change', (change) => {
-  //   console.log('Received a change event:', change);
-  // });
   console.log('Change stream created');
 
   return streamSSE(c, async (stream) => {
@@ -71,50 +64,14 @@ app.get('/stream', (c) => {
       if ('fullDocument' in change && change.fullDocument) {
         const item = change.fullDocument;
         const html = `<div class="item">${item.item}</div>`;
-        // Send the data in the correct SSE format using streamSSE
         await stream.writeSSE({
           data: html,
           event: 'message'
         });
       }
     }
-    // changeStream.on('error', (error) => {
-    //   console.error('Change stream error:', error);
-    // });
-    changeStream.on('change', async (change) => {
-      console.log('Received a change event:', change);
-      const message = `It is ${new Date().toISOString()}`
-      await stream.writeSSE({
-        data: message,
-        event: 'EventName',
-      })
-    });
   })
 })
-
-// // Streaming endpoint for live updates
-// app.get('/stream', async (c) => {
-//   console.log('Received GET request to /stream');
-//   return streamSSE(c, async (stream) => {
-//     console.log('Stream connection established');
-//     stream.onAbort(() => {
-//       console.log('Stream aborted by client');
-//     });
-
-//     changeStream.on('change', async (change) => {
-//       console.log('Received a change event in /stream:', change);
-//       if ('fullDocument' in change && change.fullDocument) {
-//         const item = change.fullDocument;
-//         const html = `<div class="item">${item.item}</div>`;
-//         // Send the data in the correct SSE format using streamSSE
-//         await stream.writeSSE({
-//           data: html,
-//           event: 'message'
-//         });
-//       }
-//     });
-//   });
-// });
 
 // Start the server
 Deno.serve(app.fetch)
